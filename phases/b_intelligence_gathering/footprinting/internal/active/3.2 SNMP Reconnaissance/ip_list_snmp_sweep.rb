e4addb1074ff1936e46ba85fa2a59f0a100be807
Rick Flores <0xnanoquetz9l@gmail.com>
2014-08-05 13:32:22 -0700

    updated file

--- phases/b_intelligence_gathering/footprinting/internal/active/3.2 SNMP Reconnaissance/ip_list_snmp_sweep.rb
+++ phases/b_intelligence_gathering/footprinting/internal/active/3.2 SNMP Reconnaissance/ip_list_snmp_sweep.rb
@@ -5,15 +5,33 @@ require 'socket'; require 'rainbow/ext/string'
 
 unless ARGV.length == 1
 			puts "Missing arguments. Also make sure your IP list has a single IP on each line (no empty or extra lines). Correct usage below:".foreground(:yellow).bright
-			puts "#{$0} ip_list.txt\n".foreground(:white).bright
+			puts "\n#{$0} ip_list.txt\n".foreground(:white).bright
 			exit
 end
+
+users = "1.3.6.1.4.1.77.1.2.25".to_s
+
 
 begin
 			list = ARGV[0]                                 # assign list variable from file taken at runtime
 			File.open(ARGV[0], "r") do |f|                 # open word list in read mode, and assign code block
 			f.each_line do |lines|                         # call each_line method, & iterate thru file like an array
-			system 'snmpwalk -c public -v1' << " #{lines}" # loop thru command on each ip in the file
+			  
+			puts "\n[+] Enumerating entire MIB tree.".foreground(:yellow).bright
+			#system 'snmpwalk -c public -v1' << " #{lines}"                                  # enumerate entire MIB tree
+			
+			puts "\n[+] Enumerating windows users.".foreground(:yellow).bright
+			system 'snmpwalk -c public -v1' << " #{lines} #{users}"        # enumerate windows users
+
+			#puts "\n[+] Enumerating running windows processes.".foreground(:yellow).bright
+			#system 'snmpwalk -c public -v1' << " #{lines}" + '1.3.6.1.2.1.25.4.2.1.2'       # enumerate running windows processes
+			
+			#puts "\n[+] Enumerating open tcp ports.".foreground(:yellow).bright
+			#system 'snmpwalk -c public -v1' << " #{lines}" + '1.3.6.1.2.1.6.13.1.3'         # enumerate open TCP ports
+			
+			#puts "\n[+] Enumerating installed software.".foreground(:yellow).bright
+			#system 'snmpwalk -c public -v1' << " #{lines}" + '1.3.6.1.2.1.25.6.3.1.2'       # enumerate installed software
+
 		end
 	end
 end
