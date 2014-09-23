9919be6c1b9ac442c302a50026158f3261d549f5
Rick Flores <0xnanoquetz9l@gmail.com>
2014-09-22 17:31:24 -0700

    adding ftp, and telnet auth scripts

---
+++ phases/b_intelligence_gathering/footprinting/internal/active/telnet.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+require 'socket'; require 'rainbow/ext/string'
+
+unless ARGV.length == 4
+  puts "\n[Telnet username / password authentication verification by Rick Flores (@nanotechz9l)]".foreground(:cyan).bright
+  puts "Missing arguments. Correct useage below:\n".foreground(:yellow).bright
+  puts "#{$0} <FTP SERVER IP> <PORT> <USERNAME> <PASSWORD>".foreground(:yellow).bright
+  puts "#{$0} 192.168.86.187 21 test test".foreground(:green).bright
+
+exit
+end
+
+ip, port, user, pass = ARGV[0..3]
+
+begin
+    s = TCPSocket.new(ARGV[0],ARGV[1])   # Create socket, connect to server + ip
+    print s.recv(2048)                   # Receive banner
+    s.puts "USER " + ARGV[2] + "\r\n"    # Verify username
+    print s.recv(1024)                   # Receive result
+    print s.recv(1024)                   # Receive result
+    s.puts ARGV[3] + "\r\n"              # Verify user password
+    print s.recv(1024)                   # Receive result
+    s.close                              # Close socket
+end
